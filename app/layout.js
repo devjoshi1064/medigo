f750d15ad018d9b656e40b5702731765d9f96f4c
devjoshi1064 <joshidev882@gmail.com>
2026-03-10 22:17:33 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,50 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({
+  subsets: ["latin"],
+})
+
+export const metadata = {
+  title: "MediMeet - Doctor Appointment Platform",
+  description: "Connect with doctors anywhere, anytime with MediMeet.",
+};
+
+<meta
+  name="format-detection"
+  content="telephone=no, date=no, email=no, address=no"
+/>
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning={true}>
+        <body
+          className={`${inter.className}`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
+            <main className="min-h-screen ">
+              {children}
+            </main>
+            {/* footer */}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,38 @@
+import { Show, SignInButton, SignUpButton, UserAvatar, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+
+const Header = () => {
+    return (
+        <header className='fixed top-0 w-full border-b bg-background/60  z-50 supports-backdrop-filter:bg-background/60'>
+            <nav className=' px-4 h-16 container mx-auto flex items-center justify-between '>
+                <Link href={"/"}>
+                    <h1 className='text-2xl font-bold'>Medi<span className='text-blue-400'>Meet</span></h1>
+                </Link>
+
+                <div>
+                    <Show when="signed-out">
+                        <SignInButton>
+                            <Button variant='secondary'>Sign In</Button>
+                        </SignInButton>
+                        
+                    </Show>
+                    <Show when="signed-in">
+                        <UserButton 
+                        appearance={{
+                            elements:{
+                                avatarBox: "w-10 h-10 rounded-full",
+                                userButtonPopoverCard: "shadow-xl",
+                                userPreviewMainIdentification: "font-semibold"
+                            }
+                        }}
+                        />
+                    </Show>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
